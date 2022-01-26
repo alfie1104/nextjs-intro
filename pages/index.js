8f737380d58cf1a6c89728aa782f03db86eb32d9
peter_alfie <alfie1104@gmail.com>
2022-01-26 09:00:31 +0900

    Dynamic routes is applied

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,45 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
 export default function Home({ results }) {
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push(
+      {
+        pathname: `/movies/${id}`,
+        query: {
+          title,
+        },
+      },
+      `/movies/${id}`
+    );
+  };
+
   return (
     <div className="container">
       <Seo title="Home" />
       {results?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-          <h4>{movie.original_title}</h4>
+        <div
+          className="movie"
+          key={movie.id}
+          onClick={() => onClick(movie.id, movie.original_title)}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>
+            <Link
+              href={{
+                pathname: `/movies/${movie.id}`,
+                query: {
+                  title: movie.original_title,
+                },
+              }}
+              as={`/movies/${movie.id}`}
+            >
+              <a>{movie.original_title}</a>
+            </Link>
+          </h4>
         </div>
       ))}
       <style jsx>
@@ -18,6 +49,9 @@ export default function Home({ results }) {
             grid-template-columns: 1fr 1fr;
             padding: 20px;
             gap: 20px;
+          }
+          .movie {
+            cursor: pointer;
           }
           .movie img {
             max-width: 100%;
@@ -39,7 +73,6 @@ export default function Home({ results }) {
 }
 
 export async function getServerSideProps() {
-  //const { results } = await (await fetch(`/api/movies`)).json();
   const { results } = await (
     await fetch(`http://localhost:3000/api/movies`)
   ).json();
